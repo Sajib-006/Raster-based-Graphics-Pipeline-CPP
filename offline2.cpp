467b2a74ad6ac611ea86b223fb17d3f599f3f2f7
Sajib-006 <1605064@ugrad.cse.buet.ac.bd>
2021-04-20 16:53:06 +0600

    translation,scale done

--- offline2.cpp
+++ offline2.cpp
@@ -5,6 +5,10 @@ struct Point
 {
 	double x = 0.0,y = 0.0,z = 0.0, w = 1.0;
 };
+struct Vector
+{
+    double x = 0.0,y = 0.0,z = 0.0, w = 1.0;
+};
 struct Matrix
 {
     /*double x1 = 1.0, x2 = 0.0, x3 = 0.0, x4 = 0.0;
@@ -40,6 +44,34 @@ struct Matrix initializeMatrix()
     m.arr[3][3] = 1.0;
     return m;
 }
+struct Matrix matrixMultiplication(struct Matrix matrix1, struct Matrix matrix2)
+{
+    struct Matrix result;
+    for(int i=0; i<4; i++){
+       for(int j=0; j<4; j++){
+            result.arr[i][j] = 0.0;
+            for(int k=0; k<4; k++)
+                result.arr[i][j] += matrix1.arr[i][k] * matrix2.arr[k][j];
+        }
+    }
+    return result;
+
+}
+struct Matrix CrossMultiplication(struct Matrix m, struct Vector v)
+{
+
+
+
+}
+
+struct Vector normalize(struct Vector a)
+{
+    double val = sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
+    a.x /= val;
+    a.y /= val;
+    a.z /= val;
+    return a;
+}
 struct Matrix genIdentityMatrix()
 {
     struct Matrix m;
@@ -58,19 +90,24 @@ struct Matrix genTranslationMatrix(double x, double y, double z)
     m.arr[2][3] = z;
     return m;
 }
-struct Matrix matrixMultiplication(struct Matrix matrix1, struct Matrix matrix2)
+struct Matrix genScaleMatrix(double x, double y, double z)
 {
-    struct Matrix result;
-    for(int i=0; i<4; i++){
-       for(int j=0; j<4; j++){
-            result.arr[i][j] = 0.0;
-            for(int k=0; k<4; k++)
-                result.arr[i][j] += matrix1.arr[i][k] * matrix2.arr[k][j];
-        }
-    }
-    return result;
+    struct Matrix m;
+    m = initializeMatrix();
+    m.arr[0][0] = x;
+    m.arr[1][1] = y;
+    m.arr[2][2] = z;
+    return m;
+}
+struct Matrix genRotationMatrixUtil(struct Vector x,double angle, struct Vector a)
+{
 
 }
+struct Matrix genRotationMatrix(double angle, struct Vector a)
+{
+
+}
+
 int main()
 {
     cout<<"Hello"<<endl;
@@ -115,9 +152,17 @@ int main()
         }
         else if(command == "scale"){
             cout<<"S"<<endl;
+            double x,y,z;
+            fin>> x >> y >> z;
+            struct Matrix T = genScaleMatrix(x,y,z);
+            s.push(matrixMultiplication(T,s.top()));
         }
         else if(command == "rotate"){
             cout<<"R"<<endl;
+            struct Vector a;
+            double angle;
+            fin>> angle >> a.x >> a.y >> a.z;
+            a = normalize(a);
         }
         else if(command == "push"){
             cout<<"Push"<<endl;
